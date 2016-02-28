e0c0190e327ef060155878571f79a1bda74c4628
Goatflakes <cian.moriarty@gmail.com>
2016-02-29 04:50:17 +1100

    Correctly generated the first plot

---
+++ plot1.R
@@ -0,0 +1,33 @@
+# load the dataset
+powerdata = read.table("household_power_consumption.txt",
+                       header = TRUE,
+                       sep = ";",
+                       na.strings = "?")
+
+# convert the strings powerdata$Date & powerdata$Time
+# into a single variable of type POSIXlt
+powerdata$Datetime = strptime(paste(powerdata$Date, " ", powerdata$Time),
+                              format = "%d/%m/%Y %H:%M:%S")
+
+# drop the now redundant columns powerdata$Data and powerdata$Time
+powerdata = subset(powerdata, select = -c(Date, Time))
+
+# subset those records from the 2007-02-01 and 2007-02-02
+# (1st and 2nd of February 2007)
+fmt = "%Y-%m-%d %H:%M:%S"
+earliest_date = strptime("2007-02-01 0:00:00", format = fmt)
+too_late_date = strptime("2007-02-03 0:00:00", format = fmt)
+powerdata = subset(powerdata,
+                   Datetime >= earliest_date & Datetime < too_late_date)
+
+# create the png device to plot too
+png(filename = "plot1.png", width = 480, height = 480)
+
+# create the plot
+hist(powerdata$Global_active_power,
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+# close the png device and write the file
+dev.off()
